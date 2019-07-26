4f532d36325f39a80630e1723f558fb0ed0b1a39
reyxz312 <53337202+reyxz312@users.noreply.github.com>
2019-07-26 18:17:05 +0800

    Add files via upload

---
+++ program/populator.py
@@ -0,0 +1,16 @@
+import recognizer as rec
+import image_generator as ig
+
+
+def start():
+    params = ["first name", "middle name", "last name",
+              "birthdate", "civil status", "citizenship", "street name where you live", 
+              "barangay where you live", "municipality or city where you live",
+              "province where you live", "precinct number"]
+    values = rec.populate(params)
+    ig.create_id(values)
+
+
+if __name__ == "__main__":
+    start()
+
